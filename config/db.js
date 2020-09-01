02970bb8ba218a268c1137b7dab7f8d5739eb3f7
moshe <moshemachluf97@gmail.com>
2020-09-01 14:36:30 +0300

    deploy

--- config/db.js
+++ config/db.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
-
+// process.env.MONGO_URI || "mongodb://localhost:27017/social-api"
 mongoose
-  .connect(process.env.MONGO_URI || "mongodb://localhost:27017/social-api", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
+  .connect(
+    "mongodb+srv://moshemachluf:wy4P6WGEN8ivMi2@postalk-api.cqdvs.mongodb.net/postalk-api?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
   .then(() => console.log("Connection to MongoDB successful!"))
   .catch(console.log);
 
